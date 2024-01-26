c57924c66e6bd76c325d76b577b5d3e7a744c71f
vboxuser <vboxuser@CPSC222.myguest.virtualbox.org>
2024-01-26 10:37:38 -0500

    new script for sudosnmp.pl

--- sudosnmp.pl
+++ sudosnmp.pl
@@ -1,11 +1,16 @@
 #!/usr/bin/perl
-
 use strict;
 use warnings;
 
-my $sudo_sessions = `journalctl | grep -c "pam_unix(sudo:session): session opened"`;
+my $auth_logs = '/var/log/auth.log'; 
 
+open my $fh, '<', $auth_logs or die "Cannot open $auth_logs: $!";
+my $sudo_sessions = 0;
+
+while (<$fh>) {
+    $sudo_sessions++ if /pam_unix\(sudo:session\): session opened/;
+}
+
+close $fh;
 chomp $sudo_sessions;
-
-print $sudo_sessions;
-
+print "$sudo_sessions";
