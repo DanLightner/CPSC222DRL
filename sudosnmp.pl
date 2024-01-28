e800ebb54bc09ef1a68963d92f25ed76d6628de0
vboxuser <vboxuser@cpsc222vbox.myguest.virtualbox.org>
2024-01-28 18:47:28 -0500

    final run through of perl script

--- sudosnmp.pl
+++ sudosnmp.pl
@@ -3,9 +3,16 @@
 use strict;
 use warnings;
 
-my $sudo_sessions = `journalctl | grep -c "pam_unix(sudo:session): session opened"`;
+my $auth_logs = '/var/log/auth.log';  # Adjust the path if needed
 
+open my $fh, '<', $auth_logs or die "Cannot open $auth_logs: $!";
+my $sudo_sessions = 0;
+
+while (<$fh>) {
+    $sudo_sessions++ if /pam_unix\(sudo:session\): session opened/;
+}
+
+close $fh;
 chomp $sudo_sessions;
-
-print $sudo_sessions;
-
+print "$sudo_sessions";
+#check
