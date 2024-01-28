01e00a4e05b41d814ac375b574723372b8a4b932
vboxuser <vboxuser@cpsc222vbox.myguest.virtualbox.org>
2024-01-28 18:51:52 -0500

    final edition of the perl script

--- sudosnmp.pl
+++ sudosnmp.pl
@@ -1,12 +1,9 @@
 #!/usr/bin/perl
+
 use strict;
 use warnings;
 
-<<<<<<< HEAD
 my $auth_logs = '/var/log/auth.log';  # Adjust the path if needed
-=======
-my $auth_logs = '/var/log/auth.log'; 
->>>>>>> c57924c66e6bd76c325d76b577b5d3e7a744c71f
 
 open my $fh, '<', $auth_logs or die "Cannot open $auth_logs: $!";
 my $sudo_sessions = 0;
@@ -18,7 +15,3 @@ while (<$fh>) {
 close $fh;
 chomp $sudo_sessions;
 print "$sudo_sessions";
-<<<<<<< HEAD
-#check
-=======
->>>>>>> c57924c66e6bd76c325d76b577b5d3e7a744c71f
